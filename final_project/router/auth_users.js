4205e00c3bbf6ffd431b8850b9b9bcd48145ceec
LiaWritesCode <liaramos.dev@outlook.com>
2025-07-01 18:59:55 -0400

    Tasks 1-7 finished, tested, confirmed working for final project

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,38 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username)=>{ 
+    return users.some(user => user.username === username)
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    return users.some(user => user.username === username && user.password === password );
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Is any information missing??
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in!" });
+    }
+
+    // Is the user authenticated???
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: "1h" });
+        // Store token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("Successfully logged in!");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Please check your details." });
+    }
 });
 
 // Add a book review
